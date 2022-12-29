03e716adc7ac22a31f739c06e8abb7e4c5962170
merryt <tymerry@gmail.com>
2022-12-29 08:45:07 -0700

    showing pops

--- Chapter1/Notes.md
+++ Chapter1/Notes.md
@@ -1,1 +1,22 @@
-This chapter is about React
+This chapter is about React. Everytime I mess with react I wonder what all the hype is about. The other tools in the space seem so strong.
+
+
+It is possible in react to build these inline react components. Idk why you would, but its an option
+
+```js const 
+App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p', null, 'Hello world, it is ', now.toString()
+    ),
+    React.createElement(
+      'p', null, a, ' plus ', b, ' is ', a + b
+    )
+  )
+}```
+
--- Chapter1/examples/part1/src/App.js
+++ Chapter1/examples/part1/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}</p>
     </div>
-  );
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Tyler" />
+    </div>
+  )
 }
 
 export default App;
